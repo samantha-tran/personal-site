cbbf5a39628ebcc806650a77c5f5e1b382350790
Samantha Tran <stran322001@gmail.com>
2021-02-25 18:08:54 +1000

    navlinks in mobile mode now close menu upon clicking

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
 const toggleButton = document.getElementsByClassName('toggle-button')[0];
-const navbarLinks = document.getElementsByClassName('navbar-links')[0];
+const navbar = document.getElementsByClassName('navbar-links')[0];
+const navLinks = document.getElementsByClassName('menu-link');
 
 toggleButton.addEventListener('click', () => {
-    navbarLinks.classList.toggle('active');
+    navbar.classList.toggle('active');
 })
+
+for (const links of navLinks) {
+    links.addEventListener('click', () => {
+        navbar.classList.remove('active');    
+    })
+}
