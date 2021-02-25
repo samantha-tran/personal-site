e760e0dd986a2c3a26b74da96455a8f8c0e82294
Samantha Tran <stran322001@gmail.com>
2021-02-25 18:25:23 +1000

    added smooth scrolling

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+
+/* NAVIGATION BAR */
+
 const toggleButton = document.getElementsByClassName('toggle-button')[0];
 const navbar = document.getElementsByClassName('navbar-links')[0];
 const navLinks = document.getElementsByClassName('menu-link');
@@ -10,4 +13,23 @@ for (const links of navLinks) {
     links.addEventListener('click', () => {
         navbar.classList.remove('active');    
     })
+}
+
+/* SMOOTH SCROLLING */
+
+const links = document.querySelectorAll(".navbar ul a");
+for (const link of links) {
+  link.addEventListener("click", clickHandler);
+}
+ 
+function clickHandler(e) {
+    if (this.classList.contains("menu-link")) {
+        e.preventDefault();
+        const href = this.getAttribute("href");
+        const offsetTop = document.querySelector(href).offsetTop;
+        scroll({
+            top: offsetTop,
+            behavior: "smooth"
+        });
+    }
 }
